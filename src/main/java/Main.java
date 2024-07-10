f2e3c0905a8e05bc59e7e30746eedb2d86b01fd1
Ben Meneghini <bencmeneghini@gmail.com>
2024-07-10 17:10:52 +1200

    Configured project to Maven. Created new tokens and replaced into .env file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,21 @@
 
 import service.Timer;
 import javax.security.auth.login.LoginException;
-
 import control.CommandController;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Main {
 
     public static JDA jda;
 
     public static void main(String[] args) throws LoginException {
-        // OTQyNTI1OTE5NjczODYwMTM2.GLFgJ-.vjInQKCMvt1vhwmqqTyriFLcrcUHaw5kKWUhfw main
-        // OTk0NzU5NDA2NjU2OTQ2MjA2.GiK5GM.bWNRWGV6sR5Xtqq9eMGAbLfqRi9IhcNxsnM2DQ test
-        JDA jda = JDABuilder.createDefault("OTQyNTI1OTE5NjczODYwMTM2.GLFgJ-.vjInQKCMvt1vhwmqqTyriFLcrcUHaw5kKWUhfw")
+        Dotenv dotenv = Dotenv.load();
+        JDA jda = JDABuilder.createDefault(dotenv.get("MAIN_BOT_TOKEN"))
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT)
                 .build();
 
